eeaec759b9540b5ac83c697f5ca39a1c59a1ac23
gleam-ru <roman.demeshenko@gmail.com>
2015-04-02 14:29:06 +0300

    сделал меню

---
+++ config/navigation.js
@@ -0,0 +1,28 @@
+module.exports.navigation = [
+    {
+        name     : "Главная",
+        href     : "/",
+        access   : "",
+        children : [],
+    },
+    {
+        name     : "Текст",
+        href     : "/",
+        access   : "",
+    },
+    {
+        name     : "Сервисы",
+        children : [
+            {
+                name     : "Первый",
+            },
+            {
+                name     : "Второй",
+            },
+        ]
+    },
+    {
+        name     : "Текущий",
+        href     : "/current",
+    }
+];
