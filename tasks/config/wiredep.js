c0687530723028a8d4bfef96d8818f71b2d88211
gleam-ru <roman.demeshenko@gmail.com>
2015-05-25 13:36:37 +0300

    убрал base href

--- tasks/config/wiredep.js
+++ tasks/config/wiredep.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     // https://github.com/taptapship/wiredep#configuration
     grunt.config.set('wiredep', {
         task: {
-            ignorePath: "../../../",
+            ignorePath: "../../..",
             exclude: ['jquery.js'],
             src: [
                 'views/layouts/parts/head.jade',
