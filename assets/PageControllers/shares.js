8bd8c29d4d8cd1ee920d82d82a6d9d76a8e042da
gleam-ru <roman.demeshenko@gmail.com>
2015-05-27 14:09:31 +0300

    альфа версия графика

--- assets/PageControllers/shares.js
+++ assets/PageControllers/shares.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
         }
         else {
             createChart(data);
-            tester();
         }
     })
     .fail(function(msg) {
@@ -25,105 +24,10 @@ $(document).ready(function() {
 
 function createChart(data) {
     var candles = data.candles;
-    // chart
-    var margin = [20, 20, 20, 50];
-    var width = $('#shares').width() - margin[1] - margin[3];
-    var height = 500 - margin[0] - margin[2];
-    // brusher
-    var b_width = $('#shares').width() - margin[3];
-    var b_height = 50;
-        margin[2] += b_height;
-
-
-    var x = techan
-        .scale
-        .financetime()
-        .range([0, width]);
-
-    var y = d3
-        .scale
-        .linear()
-        .range([height, 0]);
-
-    var y_volume = d3
-        .scale
-        .linear()
-        .range([y(0), y(0.3)]);
-
-
-
-    // Оси брашера
-    var b_x = techan
-        .scale
-        .financetime()
-        .range([0, b_width]);
-
-    var b_y = d3
-        .scale
-        .linear()
-        .range([b_height, 0]);
-
-    // поле для творчества
-    var svg = d3.select("#shares").append("svg")
-        .attr("width", width + margin[3] + margin[1])
-        .attr("height", height + margin[0] + margin[2] + b_height)
-        // .append("g")
-        // .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
-
-
-
-    // график
-    var focus = svg.append("g")
-        .attr("class", "focus")
-        .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
-
-    // видимая область
-    // чтобы не рендерить лишнее - .attr("clip-path", "url(#clip)")
-    focus
-        .append("clipPath")
-        .attr("id", "clip")
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", y(1))
-        .attr("width", width)
-        .attr("height", y(0) - y(1));
-
-
-
-    // всяко разно
-    var ctx = svg.append("g")
-        .attr("class", "ctx")
-        .attr("transform", "translate(" + margin[3] + "," + (height + margin[2]) + ")");
-
-    // ctx.append("g")
-    //     .attr("class", "close");
-
-    // ctx.append("g")
-    //     .attr("class", "x axis")
-    //     .attr("transform", "translate(0," + height2 + ")");
-
-    // ctx.append("g")
-    //     .attr("class", "y axis")
-    //     .call(yAxis);
-
-
-
-    //  ╔═╗╔═╗╔╗╔╔╦╗╦  ╔═╗╔═╗
-    //  ║  ╠═╣║║║ ║║║  ║╣ ╚═╗
-    //  ╚═╝╩ ╩╝╚╝═╩╝╩═╝╚═╝╚═╝
-    //  @candles
-
-    var candlesticks_plot = techan
-        .plot
-        .candlestick()
-        .xScale(x)
-        .yScale(y);
-
-    var accessor = candlesticks_plot.accessor();
+    // candles = candles.slice(0, 360);
+    var accessor = techan.plot.candlestick().accessor();
     var parseDate = d3.time.format("%Y-%m-%d").parse;
-
     candles = candles.map(function(c) {
-    // candles = candles.slice(0, 180).map(function(c) {
         return {
             date   : parseDate(c.date),
             open   : +c.o,
@@ -136,125 +40,192 @@ function createChart(data) {
         return d3.ascending(accessor.d(a), accessor.d(b));
     });
 
-    x.domain(candles.map(accessor.d));
-    y.domain(techan.scale.plot.ohlc(candles, accessor).domain());
+    // chart
+    var margin = [20, 0, 20, 30];
+    var width = $('#shares').width() - margin[1] - margin[3];
+    var height = 500 - margin[0] - margin[2];
 
-    focus
-        .append("g")
-        .attr("class", "candlestick")
-        .attr("clip-path", "url(#clip)")
-        .datum(candles)
+    // brusher
+    var b_width = $('#shares').width();
+    var b_height = 80;
+        margin[2] += b_height;
 
 
 
-    //  ╦  ╦╔═╗╦  ╦ ╦╔╦╗╔═╗╔═╗
-    //  ╚╗╔╝║ ║║  ║ ║║║║║╣ ╚═╗
-    //   ╚╝ ╚═╝╩═╝╚═╝╩ ╩╚═╝╚═╝
-    //   @volumes
+//  ╔═╗╦  ╦╔═╗
+//  ╚═╗╚╗╔╝║ ╦
+//  ╚═╝ ╚╝ ╚═╝
+//  @svg
+
+    // поле для творчества
+    var svg = d3.select("#shares").append("svg")
+        .attr("width", width + margin[3] + margin[1])
+        .attr("height", height + margin[0] + margin[2] + b_height)
+        // .append("g")
+        // .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
+
+
+    // график
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")")
+        .attr('opacity', 1)
+        // видимая область
+        // чтобы не рендерить лишнее - .attr("clip-path", "url(#clip)")
+        focus
+            .append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width)
+            .attr("height", height);
+
+        // объемы
+        var gVolume = focus.append("g").attr("clip-path", "url(#clip)")
+            .attr("class", "volume")
+            .datum(candles)
+
+        // свечки
+        var gCandles = focus.append("g")
+            .attr("class", "candlestick")
+            .attr("clip-path", "url(#clip)")
+            .datum(candles)
+
+        var gAxisX = focus.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")");
+
+        var gAxisY = focus.append("g")
+            .attr("class", "y axis")
+
+
+    // всяко разно
+    // (нижнаяя панель с брашером)
+    var ctx = svg.append("g")
+        .attr("class", "ctx")
+        .attr("transform", "translate(0," + (height + margin[2]) + ")");
+
+        // брашер (график)
+        var gBrusherPlot = ctx.append("g")
+            .attr("class", "close")
+            .datum(candles)
+
+        // брашер (ось времени)
+        var gBrusherX = ctx.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + b_height + ")");
+
+        var gBrusherSelection = ctx.append("g")
+            .attr("class", "brusher")
+//*/
+
+
+
+//  ╔═╗╔═╗╔╗╔╔╦╗╦  ╔═╗╔═╗
+//  ║  ╠═╣║║║ ║║║  ║╣ ╚═╗
+//  ╚═╝╩ ╩╝╚╝═╩╝╩═╝╚═╝╚═╝
+//  @candles
+
+    // время
+    var x = techan.scale
+        .financetime()
+        .domain(candles.map(accessor.d)) // TODO WTF?! oO почему тут массив из дохера элементов?!
+        .range([0, width]);
+
+    // цена
+    var y = d3.scale
+        .linear()
+        .domain(techan.scale.plot.ohlc(candles, accessor).domain())
+        .range([height, 0]);
+
+    // график
+    var candlesticks_plot = techan.plot
+        .candlestick()
+        .xScale(x)
+        .yScale(y)
+
+//*/
+
+
+
+//  ╦  ╦╔═╗╦  ╦ ╦╔╦╗╔═╗╔═╗
+//  ╚╗╔╝║ ║║  ║ ║║║║║╣ ╚═╗
+//   ╚╝ ╚═╝╩═╝╚═╝╩ ╩╚═╝╚═╝
+//  @volumes
+
+    // объемы
+    var y_volume = d3
+        .scale
+        .linear()
+        .range([y(0), y(0.2)])
+        .domain(
+            techan.scale.plot
+                .volume(candles)
+                .domain()
+        )
 
     var volumes_plot = techan
         .plot
         .volume()
         .xScale(x)
         .yScale(y_volume)
-
-    y_volume.domain(techan.scale.plot.volume(candles).domain())
-
-    focus
-        .append("g")
-        .attr("class", "volume")
-        .attr("clip-path", "url(#clip)")
-        .datum(candles)
+//*/
 
 
+//  ╔╗ ╦═╗╦ ╦╔═╗╦ ╦╔═╗╦═╗
+//  ╠╩╗╠╦╝║ ║╚═╗╠═╣║╣ ╠╦╝
+//  ╚═╝╩╚═╚═╝╚═╝╩ ╩╚═╝╩╚═
+//  @brusher
 
-    //  ╔═╗═╗ ╦╦╔═╗
-    //  ╠═╣╔╩╦╝║╚═╗
-    //  ╩ ╩╩ ╚═╩╚═╝
-    //  @axis
+    // время
+    var b_x = techan
+        .scale
+        .financetime()
+        .domain(x.domain())
+        .range([0, b_width]);
 
-    var y_axis = d3
-        .svg
-        .axis()
-        .scale(y)
-        .orient("left")
-
-    focus
-        .append("g")
-        .attr("class", "y axis")
-        // .append("text")
-        // .attr("transform", "rotate(-90)")
-        // .attr("y", 6)
-        // .attr("dy", ".71em")
-        // .style("text-anchor", "end")
-        // .text("Price ($)");
-
-    var x_axis = d3
-        .svg
-        .axis()
-        .scale(x)
-        .orient("bottom")
-
-    focus
-        .append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")");
+    // цена
+    var b_y = d3
+        .scale
+        .linear()
+        .domain(y.domain())
+        .range([b_height, 0]);
 
 
+    // график
+    gBrusherPlot
+        .call(
+            techan.plot
+                .close()
+                .xScale(b_x)
+                .yScale(b_y)
+        )
 
-/*
+    // заполняю ось данными
+    gBrusherX
+        .call(
+            d3.svg
+                .axis()
+                .scale(b_x)
+                .orient("bottom")
+        )
 
-    //  ╔╗ ╦═╗╦ ╦╔═╗╦ ╦╔═╗╦═╗
-    //  ╠╩╗╠╦╝║ ║╚═╗╠═╣║╣ ╠╦╝
-    //  ╚═╝╩╚═╚═╝╚═╝╩ ╩╚═╝╩╚═
-    //  @brusher
-
-    b_x.domain(x.domain())
-    b_y.domain(y.domain())
-
-
-    var close = techan
-        .plot
-        .close()
-        .xScale(b_x)
-        .yScale(b_y);
-    ctx
-        .append("g")
-        .attr("class", "close")
-    ctx
-        .select("g.close")
-        .datum(candles)
-        .call(close)
-
-
-    var b_x_axis = d3
-        .svg
-        .axis()
-        .scale(x)
-        .orient("bottom")
-    ctx
-        .append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + b_height + ")");
-    ctx
-        .select("g.x.axis")
-        .call(b_x_axis)
-
-
-    var brush = d3
-        .svg
+    // сам брашер.
+    var brush = d3.svg
         .brush()
-        // .on("brush", draw);
+        .on("brushend", brushended);
 
-    var plot_data    = x.zoomable();
-    var brusher_data = b_x.zoomable();
+    brush.x(
+        d3.scale
+            .linear()
+            // если я правильно понял, то zoomable превращает
+            // набор данных оси в "номера свечек"
+            .domain(b_x.zoomable().domain())
+            .range(b_x.range())
+    )
 
-    brush.x(brusher_data)
-    ctx
-        .append("g")
-        .attr("class", "brusher")
-    ctx
-        .select("g.brusher")
+    gBrusherSelection
         .call(brush)
         .selectAll("rect")
         .attr("height", b_height)
@@ -262,138 +233,105 @@ function createChart(data) {
 
 
 
+//  ╔═╗═╗ ╦╦╔═╗
+//  ╠═╣╔╩╦╝║╚═╗
+//  ╩ ╩╩ ╚═╩╚═╝
+//  @axis
+
+    var x_axis_data = d3
+        .svg
+        .axis()
+        .scale(x)
+        .orient("bottom")
+
+    var y_axis_data = d3
+        .svg
+        .axis()
+        .scale(y)
+        .orient("left")
+//*/
 
 
 
+//  ╔═╗╔═╗╔═╗╔═╗
+//  ╠╣ ║ ║║ ║╚═╗
+//  ╚  ╚═╝╚═╝╚═╝
+//  @foo
 
 
-
-
-
-
-
-
-    var dates = x.domain();
-    var _x = techan
-        .scale
-        .financetime()
-        .domain(x.domain())
-    // var _x = d3
-    //     .time
-    //     .scale()
-    //     .domain([dates[0], dates[dates.length - 1]])
-    //     .domain([new Date(2006, 1, 1), new Date()])
-        .range([0, b_width]);
-
-    var brush = d3.svg.brush();
-    var brusher_data = _x.zoomable();
-    brush.x(brusher_data)
-
-    brush.on("brushend", brushended);
-
-
-    // bg
-    ctx.append("rect")
-        .attr("class", "grid-background")
-        .attr("width", b_width)
-        .attr("height", b_height);
-
-    // "cols"
-    ctx.append("g")
-        .attr("class", "x grid")
-        .attr("transform", "translate(0," + b_height + ")")
-        .call(d3.svg.axis()
-            .scale(_x)
-            .orient("bottom")
-            .ticks(d3.time.month, 6)
-            .tickSize(-b_height)
-            .tickFormat(""))
-      .selectAll(".tick")
-        .classed("minor", function(d) {
-            return d.getMonth() === 6;
-        });
-
-    // заполняем данными
-    ctx
-        .append("g")
-        .attr("class", "brush")
-        .call(brush)
-        .call(brush.event)
-        .selectAll("rect")
-        .attr("height", b_height);
-
+    // @brushended
+    // валидация данных брашера
     function brushended() {
-        // transition only after input
+        // only transition after input
         if (!d3.event.sourceEvent) return;
 
-        var extent = brush.extent().map(function(d) {
-            d.setDate(1);
-            if (d.getMonth() >= 6) {
-                // вторая половина года
-                d.setMonth(6);
-            }
-            else {
-                d.setMonth(0);
-            }
-            return d3.time.year(d);
-        });
-
-        // вероятно, это просто клик
-        if (extent[0] >= extent[1]) {
-            extent[1].addMonths(6);
-        }
-        if (extent[1].getUTCFullYear() === (new Date()).getUTCFullYear()) {
-            extent[1].addMonths(6);
+        var extent = brush.extent(); //.map(Math.round);
+        // был просто клик
+        if (brush.empty()) { // Math.abs(extent[0] - extent[1]) < 180) {
+            extent[1] = extent[0] + 180;
         }
 
-        d3
-            .select(this) // brush?
+        // анимация при выборе другого диапазона
+        focus
             .transition()
+            .duration(350)
+            .attr('opacity', 0)
+            .each('end', function() {
+                draw()
+                focus
+                    .transition()
+                    .duration(200)
+                    .attr('opacity', 1)
+            })
+
+        // анимация "удлинения" выбранного диапазона
+        // (например при клике)
+        d3
+            .select(this)
+            .transition()
+            .duration(300)
             .call(brush.extent(extent))
-            .call(brush.event);
+            .call(brush.event)
+            // .each('end', draw) // без анимации выглядит как тормоза...
 
-        draw();
+        // draw()
     }
 
 
-
-
-
-
-
-
-
-
-
-
-    var plot_data    = x.zoomable();
-
-
+    // @draw
+    // отрисовывает видимые данные
     function draw() {
-
         // привязка данных брашера к данным графика
-        // var plot_data = x.zoomable();
-        // plot_data.domain([0, 200]);
-        plot_data.domain(brush.empty() ? _x.domain() : brush.extent())
+        // https://github.com/andredumas/techan.js/blob/54e14442e30d7bd779e8fd4d9cddd25dc69a3cb6/src/scale/zoomable.js
+        var visibleCandlesRange = brush.empty() ? brush.x().domain() : brush.extent();
+        x // ось
+            .zoomable() // получить связанный набор свечек
+            .domain(visibleCandlesRange) // установить видимые свечи
 
-        focus
-            .select("g.volume")
-            // .call(volumes_plot)
-        focus
-            .select("g.candlestick")
+
+        // изменить Y ось, согласно видимым свечам
+        var data = gCandles.datum();
+        // типо строим виртуальный график, получаем его домен
+        // и подставляем его в реальный график
+        y.domain(
+            techan.scale.plot.ohlc(
+                data
+                    .slice // делаем копию, как я понял (чтобы не побить исходник)
+                    .apply(data, visibleCandlesRange),
+                accessor // яхз что это
+            )
+            .domain()
+        )
+
+
+        // drawing
+        gAxisX.call(x_axis_data)
+        gAxisY.call(y_axis_data)
+        gVolume.call(volumes_plot)
+        gCandles
+            // .transition() // techan пока что (27.05.2014) так не умеет
             .call(candlesticks_plot)
-        focus
-            .select("g.y.axis")
-            .call(y_axis)
-        focus
-            .select("g.x.axis")
-            .call(x_axis)
     }
-
     draw();
+//*/
 }
-
-
-function tester() {
-}
-
