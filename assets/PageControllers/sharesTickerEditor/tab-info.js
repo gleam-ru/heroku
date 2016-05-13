696ac1a0a8ab9fb07113ed34921bee7091346a3b
demeshenko <demeshenko@neolant.ru>
2016-05-13 13:58:39 +0300

    info tab added

--- assets/PageControllers/sharesTickerEditor/tab-info.js
+++ assets/PageControllers/sharesTickerEditor/tab-info.js
@@ -12,7 +12,37 @@ module.exports = function(resolve) {
         return {
             template: [
                 '<div>',
-                    '<div>tab-info</div>',
+                    '<ul>',
+                        '<li>',
+                            '<a',
+                                'href="http://mfd.ru/marketdata/ticker/?id={{info.mfd_id}}"',
+                                'target="_blank"',
+                                'class="mr-10"',
+                                '>',
+                                '<i class="fa fa-external-link"></i>',
+                            '</a>',
+                            'MFD_id: {{info.mfd_id}}',
+                        '</li>',
+                        '<li>',
+                            '<a',
+                                'href="/services/shares/{{ticker.id}}/info"',
+                                'target="_blank"',
+                                'class="mr-10"',
+                                '>',
+                                '<i class="fa fa-external-link"></i>',
+                            '</a>',
+                            'Всего свечей: {{info.candlesCount}}',
+                        '</li>',
+                        '<li>',
+                            'Крайняя daily свеча: {{info.lastDailyDate}}',
+                        '</li>',
+                        '<li>',
+                            'Крайняя inday свеча: {{info.lastIndayDate}}',
+                        '</li>',
+                        '<li>',
+                            'Количество сегодняшних свечей: {{info.indayCount}}',
+                        '</li>',
+                    '</ul>',
                 '</div>',
             ].join(' '),
             //
@@ -20,7 +50,16 @@ module.exports = function(resolve) {
             //
             props: [],
             data: function() {
+                var info = {};
+                info.id = ticker.id;
+                info.mfd_id = ticker.mfd_id;
+                info.candlesCount = ticker.info.candlesCount;
+                info.lastDailyDate = ticker.info.lastDay;
+                info.lastIndayDate = ticker.info.lastCandle && ticker.info.lastCandle.date ? ticker.info.lastCandle.date : '...';
+                info.indayCount = ticker.info.indayCount ? ticker.info.indayCount : 0;
                 return {
+                    info: info,
+                    ticker: ticker,
                 }
             },
             computed: {
@@ -37,3 +76,5 @@ module.exports = function(resolve) {
     .then(resolve)
     ;
 };
+
+
