81bb58d7d9c4ab6c22e26d7e48e04ad57ed3f34f
gleam-ru <roman.demeshenko@gmail.com>
2016-07-07 12:09:51 +0300

    отображение времени до обновления indayCandles

--- api/controllers/FavoritesController.js
+++ api/controllers/FavoritesController.js
@@ -8,9 +8,9 @@ module.exports = {
 	index: function(req, res) {
         var data = {
             title: 'Избранное',
+            nextUpdate: cron.tasks.sharesInday ? moment(cron.tasks.sharesInday.next()).fromNow() : '!!! обновление отключено !!!',
         };
         return res.render('favorites', data);
     },
 
 };
-
